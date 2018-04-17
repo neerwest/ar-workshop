a1c6084477c9e2788663746456d49ad654eb7480
Mauricio Siller <msyler@MacBook-Pro-de-Mauricio.local>
2018-04-16 21:09:12 -0500

    first-commit

---
+++ Scripts/CarMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float FowardForce = 40f;
+	public float BackwardsForce = 20f;
+	public float MaxForwardVelocity = 20f;
+	public float MaxBackwardsVelocity = 10f;
+	public float SteerVelocity = 60f;
+
+	// Use this for initialization
+	void Start () {
+		this.rb = this.gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKey(KeyCode.W)) {
+			this.rb.AddRelativeForce (Vector3.forward * this.FowardForce);
+			Debug.Log (this.rb.velocity);
+			if (this.rb.velocity.z > MaxForwardVelocity) {
+				this.rb.velocity = new Vector3 (0f, 0f, MaxForwardVelocity); 
+			}
+		}
+
+		if(Input.GetKey(KeyCode.S)) {
+			this.rb.AddRelativeForce (-Vector3.forward * this.BackwardsForce);
+			if (this.rb.velocity.z < -this.MaxBackwardsVelocity) {
+				this.rb.velocity = new Vector3 (0f, 0f, -this.MaxBackwardsVelocity); 
+			}
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate(Vector3.up, -this.SteerVelocity * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(Vector3.up, +this.SteerVelocity * Time.deltaTime);
+		}
+
+	}
+}
